9a40fdeb18692fe527e9c6959e387b86474d5598
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2021-11-16 20:52:31 +0400

    Revert "Revert "Added project for lab13""
    This reverts commit 223751f89eda34ce10e96280d544508f1237b2d4.

---
+++ Algorithms2_lab/Lab13StringUtils/string.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdlib.h>
+
+size_t lh_strlen(const char* string);
+void lh_strcpy(char* destination, const char* source);
+void lh_strcat(char* destination, const char* source);
