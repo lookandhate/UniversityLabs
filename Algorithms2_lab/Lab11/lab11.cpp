c33a1e16aea6dac437945dab3b7f657b4a594a3b
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2022-01-17 20:38:04 +0400

    probably fixed lab 11(again)

--- Algorithms2_lab/Lab11/lab11.cpp
+++ Algorithms2_lab/Lab11/lab11.cpp
@@ -4,9 +4,14 @@
 #include <cassert>
 
 #define _CRT_SECURE_NO_WARNINGS
+
+#define INPUT_FILE "E:\\ULSTU\\Programming and algorithms\\testTextFiles\\lab11\\testReadData.txt"
+#define OUTPUT_HTML_DOC "E:\\ULSTU\\Programming and algorithms\\testTextFiles\\lab11\\output.html"
 #pragma warning(disable:4996)
 
-void readDataFromTextFile(const char* textFilePath,char** &buffer, int& bufferSize)
+
+
+void readDataFromTextFile(const char* textFilePath, char**& buffer, int& bufferSize)
 {
 	// Check if buffer initializaited
 	if (!buffer)
@@ -14,7 +19,7 @@ void readDataFromTextFile(const char* textFilePath,char** &buffer, int& bufferSi
 		buffer = (char**)malloc(sizeof(char*) * 32); // allocating memory for array of 32 pointers to char pointer
 		printf("Memory for buffer allocated at 0x%p\n", buffer);
 	}
-	
+
 	if (buffer == nullptr)
 	{
 		printf("Buffer is nullptr after allocating!\n");
@@ -63,33 +68,35 @@ int main()
 {
 	// Pointer to array of strings
 	char** buffer = (char**)malloc(sizeof(char**));
-	
+
 	const char* boldNOPWord = "<i><b>NOP</b></i>";
 
 	int bufferTrueSize = 0;
-	readDataFromTextFile("C:\\Users\\root\\Desktop\\labs\\testTextFiles\\lab11\\testReadData.txt",
+	readDataFromTextFile(INPUT_FILE,
 		buffer, bufferTrueSize);
-	
+
 	for (size_t i = 0; i < bufferTrueSize; i++)
 	{
 		printf("buffer[%d] = %s\n", i, buffer[i]);
-		
+
 		char* copyOfCurrentString = (char*)malloc(sizeof(char*) * 255);
 		char* newString = (char*)malloc(sizeof(char*) * 255);
 		strcpy(newString, "\0");
 		strcpy(copyOfCurrentString, buffer[i]);
-		
+
 		char* token = strtok(copyOfCurrentString, " ");
 		while (token != NULL)
 		{
-			printf("\t token = %s; buffer[%d] is %s\n", token, i, buffer[i]);
+			printf("\t token = %s;\n", token);
 			if (isAInString(token))
 			{
+				printf("\t there is a A in the string, replacing it's with a NOP\n\n");
 				strcat(newString, boldNOPWord);
 				strcat(newString, " ");
 			}
 			else
 			{
+				printf("\t there are no As in the string -> not doing anything\n\n");
 				strcat(newString, token);
 				strcat(newString, " ");
 			}
@@ -100,9 +107,9 @@ int main()
 		// Removes /n from the end of string(buffer[i])
 		buffer[i][strcspn(buffer[i], "\n")] = 0;
 	}
-	
+
 	FILE* outputFilePtr;
-	int outputFileOpenError = fopen_s(&outputFilePtr, "C:\\Users\\root\\Desktop\\labs\\testTextFiles\\lab11\\output.html", "wt");
+	int outputFileOpenError = fopen_s(&outputFilePtr, OUTPUT_HTML_DOC, "wt");
 	if (outputFileOpenError)
 	{
 		printf("Could not open text file for writing output data! Error code %d\n", outputFileOpenError);
@@ -113,14 +120,15 @@ int main()
 	fprintf(outputFilePtr, "<HTML>\n\t<HEAD>\n\t\t<TITLE>%s</TITLE>\n\t</HEAD>\n", buffer[0]);
 	fprintf(outputFilePtr, "\t<BODY>\n");
 	fprintf(outputFilePtr, "\t\t<H1>%s</H1>\n", buffer[0]);
-	
+
 	for (size_t i = 1; i < bufferTrueSize; i++)
 	{
 		fprintf(outputFilePtr, "\t\t%s <BR>\n", buffer[i]);
 	}
-	
+
 	fprintf(outputFilePtr, "\t</BODY>\n");
 	fprintf(outputFilePtr, "</HTML>\n");
 	fclose(outputFilePtr);
+
 
 }
