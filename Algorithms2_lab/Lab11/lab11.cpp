ee75b4991a13dc5b37c74c3e86fef991d164a816
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2022-01-17 20:36:12 +0400

    probably fucking fixed lab11

--- Algorithms2_lab/Lab11/lab11.cpp
+++ Algorithms2_lab/Lab11/lab11.cpp
@@ -4,7 +4,12 @@
 #include <cassert>
 
 #define _CRT_SECURE_NO_WARNINGS
+
+#define INPUT_FILE "E:\\ULSTU\\Programming and algorithms\\testTextFiles\\lab11\\testReadData.txt"
+#define OUTPUT_HTML_DOC "E:\\ULSTU\\Programming and algorithms\\testTextFiles\\lab11\\output.html"
 #pragma warning(disable:4996)
+
+
 
 void readDataFromTextFile(const char* textFilePath,char** &buffer, int& bufferSize)
 {
@@ -67,7 +72,7 @@ int main()
 	const char* boldNOPWord = "<i><b>NOP</b></i>";
 
 	int bufferTrueSize = 0;
-	readDataFromTextFile("C:\\Users\\root\\Desktop\\labs\\testTextFiles\\lab11\\testReadData.txt",
+	readDataFromTextFile(INPUT_FILE,
 		buffer, bufferTrueSize);
 	
 	for (size_t i = 0; i < bufferTrueSize; i++)
@@ -82,14 +87,16 @@ int main()
 		char* token = strtok(copyOfCurrentString, " ");
 		while (token != NULL)
 		{
-			printf("\t token = %s; buffer[%d] is %s\n", token, i, buffer[i]);
+			printf("\t token = %s;\n", token);
 			if (isAInString(token))
 			{
+				printf("\t there is a A in the string, replacing it's with a NOP\n\n");
 				strcat(newString, boldNOPWord);
 				strcat(newString, " ");
 			}
 			else
 			{
+				printf("\t there are no As in the string -> not doing anything\n\n");
 				strcat(newString, token);
 				strcat(newString, " ");
 			}
@@ -102,7 +109,7 @@ int main()
 	}
 	
 	FILE* outputFilePtr;
-	int outputFileOpenError = fopen_s(&outputFilePtr, "C:\\Users\\root\\Desktop\\labs\\testTextFiles\\lab11\\output.html", "wt");
+	int outputFileOpenError = fopen_s(&outputFilePtr, OUTPUT_HTML_DOC, "wt");
 	if (outputFileOpenError)
 	{
 		printf("Could not open text file for writing output data! Error code %d\n", outputFileOpenError);
@@ -120,7 +127,8 @@ int main()
 	}
 	
 	fprintf(outputFilePtr, "\t</BODY>\n");
-	fprintf(outputFilePtr, "</HTML>\n");
+	fprintf(outputFilePtr, "</HTML>\n");    
 	fclose(outputFilePtr);
+
 
 }
